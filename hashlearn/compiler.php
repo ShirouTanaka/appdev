d0589bdae86b5995367158a685e5302745ee2c18
g310-neo <g310.deguzman@gmail.com>
2022-04-22 23:02:26 +0800

    integrated compiler and get_file

--- hashlearn/compiler.php
+++ hashlearn/compiler.php
@@ -11,6 +11,20 @@
   
         <textarea name="initScript" 
             rows="8" cols="80">
+            <?php
+                //should work in tandem with the compiler.php where this one would retrieve the file and enter the contents
+                include 'connect.php';
+
+                $sql_query = $con->query("SELECT * FROM submissions ORDER BY uploaded_on DESC");
+
+                if($sql_query->num_rows > 0){
+                    while($row = $sql_query->fetch_assoc()){
+                        $file_path = "uploads/".$row["file_name"];
+                        echo readfile($file_path);
+                    }
+                }
+                    
+            ?>
         </textarea>
           
         <input type="submit" value="Test">
